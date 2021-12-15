31a621f7a5549029ddc3c0d97281cebb478a0d28
Marcos Wlrich <marcosapw.dev@gmail.com>
2021-12-15 01:21:03 -0300

    Validando CPF e conta

--- src/index.js
+++ src/index.js
@@ -10,16 +10,28 @@ const customers = []
 app.post('/account', (req, res) => {
   const { cpf, name } = req.body
 
-  const id = uuidv4()
+  const customerAlreadyExist = customers.some(customer => customer.cpf === cpf)
+
+  if (customerAlreadyExist) {
+    return res.status(400).json({ error: 'Customer already exist!' })
+  }
 
   customers.push({
     cpf,
     name,
-    id,
+    id: uuidv4(),
     statement: []
   })
 
   return res.status(201).send()
+})
+
+app.get('/statement/:cpf', (req, res) => {
+  const { cpf } = req.params
+
+  const customer = customers.find(customer => customer.cpf === cpf)
+
+  return res.json(customer.statement)
 })
 
 app.listen(3333)
